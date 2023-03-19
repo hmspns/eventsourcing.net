b60ce96db6ff069de08b4c530f6ad48bd2cf5f97
Pavel Gribov <pavel@gribov.pro>
2023-03-19 22:05:44 +0300

    Improvements to JsonSerializer

--- EventSourcing.Serialization.Newtonsoft/JsonPayloadSerializer.cs
+++ EventSourcing.Serialization.Newtonsoft/JsonPayloadSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Buffers;
+using System.Text;
 using EventSourcing.Abstractions;
 using Newtonsoft.Json;
 
@@ -22,19 +23,77 @@ public class JsonPayloadSerializer : IPayloadSerializer
     
     public byte[] Serialize(object obj, out string type)
     {
+        if (obj == null)
+        {
+            throw new ArgumentNullException(nameof(obj));
+        }
+        
         type = obj.GetType().FullName;
-        string raw = JsonConvert.SerializeObject(obj, _settings);
-        return Encoding.UTF8.GetBytes(raw);
+        return SerializeJson(obj);
     }
 
     public object Deserialize(byte[] data, string type)
     {
+        if (data == null)
+        {
+            throw new ArgumentNullException(nameof(data));
+        }
+
+        if (string.IsNullOrWhiteSpace(type))
+        {
+            throw new ArgumentException("Type must be not an empty string", nameof(type));
+        }
+        
         if (!_mappings.TryGetValue(type, out Type t))
         {
             throw new ArgumentOutOfRangeException(nameof(type), "Couldn't proper type for Event");
         }
-        string raw = Encoding.UTF8.GetString(data);
-        object result = JsonConvert.DeserializeObject(raw, t, _settings);
-        return result;
+
+        return DeserializeJson(t, data);
+    }
+
+    private byte[] SerializeJson(object obj)
+    {
+        using MemoryStream ms = new MemoryStream();
+        using TextWriter tw = new StreamWriter(ms);
+        using JsonTextWriter jw = new JsonTextWriter(tw)
+        {
+            ArrayPool = JsonArrayPool.Instance
+        };
+        JsonSerializer serializer = JsonSerializer.CreateDefault(_settings);
+        serializer.Serialize(jw, obj);
+        return ms.ToArray();
+    }
+    
+    private object DeserializeJson(Type objectType, byte[] content)
+    {
+        using MemoryStream ms = new MemoryStream(content);
+        using TextReader tr = new StreamReader(ms);
+        using JsonTextReader jr = new JsonTextReader(tr)
+        {
+            ArrayPool = JsonArrayPool.Instance
+        };
+        JsonSerializer serializer = JsonSerializer.CreateDefault(_settings);
+        return serializer.Deserialize(jr, objectType);
+    }
+}
+
+internal sealed class JsonArrayPool : IArrayPool<char>
+{
+    private static readonly JsonArrayPool _instance = new JsonArrayPool();
+
+    internal static JsonArrayPool Instance => _instance;
+        
+    public char[] Rent(int minimumLength)
+    {
+        return ArrayPool<char>.Shared.Rent(minimumLength);
+    }
+
+    public void Return(char[]? array)
+    {
+        if (array != null)
+        {
+            ArrayPool<char>.Shared.Return(array);
+        }
     }
 }
