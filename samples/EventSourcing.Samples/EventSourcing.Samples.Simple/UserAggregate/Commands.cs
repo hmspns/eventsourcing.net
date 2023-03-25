c0efaef9b6355b348daf7f0a2435a8ef1f358f10
Pavel Gribov <pavel@gribov.pro>
2023-03-26 01:52:48 +0300

    Moved samples project

--- samples/EventSourcing.Samples/EventSourcing.Samples.Simple/UserAggregate/Commands.cs
+++
@@ -1,9 +0,0 @@
-﻿using EventSourcing.Abstractions.Contracts;
-
-namespace EventSourcing.Samples.Simple.UserAggregate;
-
-public record CreateUserCommand(string Name, DateTime BirthDate, string PhoneNumber) : ICommand;
-
-public record UpdateUserCommand(string Name, string PhoneNumber) : ICommand;
-
-public record DeleteUserCommand() : ICommand;
