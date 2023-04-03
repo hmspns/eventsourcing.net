72aab5287d6c926e321ae1db2f68438c3c8bc5e7
Pavel Gribov <pavel@gribov.pro>
2023-04-04 02:18:18 +0300

    Improvements for redis

--- src/EventSourcing.Storage.Redis/DI/EventSourcingOptionsExtensions.cs
+++ src/EventSourcing.Storage.Redis/DI/EventSourcingOptionsExtensions.cs
@@ -48,29 +48,3 @@ public static class EventSourcingOptionsExtensions
         return new RedisOptions(options._services);
     }
 }
-
-public sealed class RedisOptions
-{
-    private readonly IServiceCollection _services;
-
-    internal RedisOptions(IServiceCollection services)
-    {
-        _services = services;
-    }
-
-    /// <summary>
-    /// Use custom IRedisKeyGenerator.
-    /// </summary>
-    /// <param name="keyGenerator">Implementation of IRedisKeyGenerator.</param>
-    /// <returns>Options.</returns>
-    /// <remarks>
-    /// Service will be registered as singleton.
-    /// Implementation of IRedisKeyGenerator should be thread-safe.
-    /// </remarks>
-    public RedisOptions UseRedisKeyGenerator(IRedisKeyGenerator keyGenerator)
-    {
-        _services.Remove<IRedisKeyGenerator>();
-        _services.AddSingleton<IRedisKeyGenerator>(keyGenerator);
-        return this;
-    }
-}
---
+++ src/EventSourcing.Storage.Redis/DI/RedisOptions.cs
@@ -0,0 +1,30 @@
+﻿using EventSourcing.Net;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventSourcing.Storage.Redis;
+
+public sealed class RedisOptions
+{
+    private readonly IServiceCollection _services;
+
+    internal RedisOptions(IServiceCollection services)
+    {
+        _services = services;
+    }
+
+    /// <summary>
+    /// Use custom IRedisKeyGenerator.
+    /// </summary>
+    /// <param name="keyGenerator">Implementation of IRedisKeyGenerator.</param>
+    /// <returns>Options.</returns>
+    /// <remarks>
+    /// Service will be registered as singleton.
+    /// Implementation of IRedisKeyGenerator should be thread-safe.
+    /// </remarks>
+    public RedisOptions UseRedisKeyGenerator(IRedisKeyGenerator keyGenerator)
+    {
+        _services.Remove<IRedisKeyGenerator>();
+        _services.AddSingleton<IRedisKeyGenerator>(keyGenerator);
+        return this;
+    }
+}
