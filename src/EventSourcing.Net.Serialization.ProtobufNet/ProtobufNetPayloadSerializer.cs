ff0dc9876297b9ab49f150056d8c0229a8de4b86
Pavel Gribov <pavel@gribov.pro>
2023-05-08 04:09:49 +0300

    Scaffolding for protobus.net

---
+++ src/EventSourcing.Net.Serialization.ProtobufNet/ProtobufNetPayloadSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Buffers;
+using EventSourcing.Net.Abstractions.Contracts;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace EventSourcing.Net.Serialization.ProtobufNet;
+
+public sealed class ProtobufNetPayloadSerializer : IPayloadSerializer
+{
+    public byte[] Serialize(object obj)
+    {
+        using MemoryStream ms = new MemoryStream();
+        Serializer.Serialize(ms, obj);
+        return ms.ToArray();
+    }
+
+    public object Deserialize(Type type, Memory<byte> data)
+    {
+        object? result = RuntimeTypeModel.Default.Deserialize(type, data);
+        return result;
+    }
+}
