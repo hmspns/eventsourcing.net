1b7a770141ca2bca315c2d2e1c27ac4669afc649
Pavel Gribov <pavel@gribov.pro>
2023-05-11 02:21:48 +0300

    Added pooled stream

---
+++ src/EventSourcing.Net.Engine/Pooled/PooledMemoryBufferWriter.cs
@@ -0,0 +1,214 @@
+﻿/*
+ * Copyright 2017 itn3000
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using EventSourcing.Net.Engine.Exceptions;
+
+namespace EventSourcing.Net.Engine.Pooled;
+
+/// <summary>
+/// Represents an pooled output sink into which <typeparam name="T"/> data can be written.
+/// </summary>
+public sealed class PooledMemoryBufferWriter<T> : IDisposable, IBufferWriter<T> where T : struct
+{
+    private readonly ArrayPool<T> _pool;
+    private T[] _currentBuffer;
+    private int _position;
+    private int _length;
+    private const int DEFAULT_SIZE = 1024;
+    private bool _isDisposed;
+
+    /// <summary>
+    /// Initialize new object.
+    /// </summary>
+    /// <param name="initialCapacity">Initial capacity</param>
+    public PooledMemoryBufferWriter(int initialCapacity = DEFAULT_SIZE)
+        : this(ArrayPool<T>.Shared, initialCapacity)
+    {
+    }
+
+    /// <summary>
+    /// Initialize new object.
+    /// </summary>
+    /// <param name="pool">Array pool.</param>
+    /// <param name="initialCapacity">initial reserved buffer size.</param>
+    public PooledMemoryBufferWriter(ArrayPool<T> pool, int initialCapacity = DEFAULT_SIZE)
+    {
+        CheckDisposed();
+        if (pool == null)
+        {
+            Thrown.ArgumentNullException(nameof(pool));
+        }
+
+        if (initialCapacity < 0)
+        {
+            Thrown.ArgumentOutOfRangeException(nameof(initialCapacity), "Size must be greater than 0");
+        }
+
+        _pool = pool;
+        _position = 0;
+        _length = 0;
+        ResizeBuffer(initialCapacity);
+    }
+
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Advance(int count)
+    {
+        CheckDisposed();
+        if (_position + count > _currentBuffer.Length)
+        {
+            Thrown.ArgumentOutOfRangeException(nameof(count), "Current position + count will overflow buffer size");
+        }
+
+        _position += count;
+        if (_length < _position)
+        {
+            _length = _position;
+        }
+    }
+
+    /// <summary>return buffer to pool and reset buffer status</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Dispose()
+    {
+        if (_isDisposed)
+        {
+            return;
+        }
+        
+        if (_currentBuffer != null)
+        {
+            _pool.Return(_currentBuffer);
+            _currentBuffer = null;
+            _position = 0;
+            _length = 0;
+        }
+
+        _isDisposed = true;
+    }
+
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Memory<T> GetMemory(int sizeHint = 0)
+    {
+        CheckDisposed();
+        if (sizeHint < 0)
+        {
+            Thrown.ArgumentOutOfRangeException(nameof(sizeHint), "Size must be greater than 0");
+        }
+
+        if (sizeHint == 0)
+        {
+            sizeHint = DEFAULT_SIZE;
+        }
+
+        if (_position + sizeHint > _currentBuffer.Length)
+        {
+            ResizeBuffer(_position + sizeHint);
+        }
+
+        return _currentBuffer.AsMemory(_position, sizeHint);
+    }
+
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Span<T> GetSpan(int sizeHint = 0)
+    {
+        CheckDisposed();
+        if (sizeHint < 0)
+        {
+            Thrown.ArgumentOutOfRangeException(nameof(sizeHint), "Size must be greater than 0");
+        }
+
+        if (sizeHint == 0)
+        {
+            sizeHint = DEFAULT_SIZE;
+        }
+
+        if (_position + sizeHint > _currentBuffer.Length)
+        {
+            ResizeBuffer(_position + sizeHint);
+        }
+
+        return _currentBuffer.AsSpan(_position, sizeHint);
+    }
+
+    /// <summary>Get data as Span.</summary>
+    /// <remarks>You must not use returned value outside of stream's lifetime</remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadOnlySpan<T> ToSpanUnsafe()
+    {
+        CheckDisposed();
+        return _currentBuffer.AsSpan(0, _length);
+    }
+
+    /// <summary>Get data as Span.</summary>
+    /// <remarks>You must not use returned value outside of stream's lifetime.</remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadOnlyMemory<T> ToMemoryUnsafe()
+    {
+        CheckDisposed();
+        return _currentBuffer.AsMemory(0, _length);
+    }
+
+    /// <summary>Reset buffer status.</summary>
+    /// <remarks>New buffer with <paramref name="preallocateSize"/> size will be fetched from pool.</remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Reset(int preallocateSize)
+    {
+        CheckDisposed();
+        if (preallocateSize < 0)
+        {
+            Thrown.ArgumentOutOfRangeException(nameof(preallocateSize), "Size must be greater than 0");
+        }
+
+        _pool.Return(_currentBuffer);
+        _currentBuffer = _pool.Rent(preallocateSize);
+        _length = 0;
+        _position = 0;
+    }
+
+    /// <summary>Reset buffer status.</summary>
+    /// <remarks>Buffer will be reused.</remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Reset()
+    {
+        CheckDisposed();
+        _length = 0;
+        _position = 0;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [System.Diagnostics.CodeAnalysis.MemberNotNull(nameof(_currentBuffer))]
+    private void ResizeBuffer(int sizeHint)
+    {
+        T[] tmp = _pool.Rent(sizeHint);
+        if (_currentBuffer != null)
+        {
+            Buffer.BlockCopy(_currentBuffer, 0, tmp, 0,
+            _currentBuffer.Length < tmp.Length ? _currentBuffer.Length : tmp.Length);
+            _pool.Return(_currentBuffer);
+        }
+
+        _currentBuffer = tmp;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void CheckDisposed()
+    {
+        if (_isDisposed)
+        {
+            Thrown.ObjectDisposedException(nameof(PooledMemoryStream));
+        }
+    }
+}
---
+++ src/EventSourcing.Net.Engine/Pooled/PooledMemoryStream.cs
@@ -0,0 +1,328 @@
+﻿/*
+ * Copyright 2017 itn3000
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Buffers;
+using System.IO;
+using System.Runtime.CompilerServices;
+using EventSourcing.Net.Engine.Exceptions;
+
+namespace EventSourcing.Net.Engine.Pooled;
+
+/// <inheritdoc />
+public sealed class PooledMemoryStream : Stream
+{
+    private readonly ArrayPool<byte>? _pool;
+    private byte[] _currentBuffer;
+    private int _length;
+    private int _position;
+    private bool _isDisposed;
+    
+    /// <summary>Initialize new object.</summary>
+    public PooledMemoryStream()
+        : this(ArrayPool<byte>.Shared)
+    {
+    }
+
+    /// <summary>Initialize new object with specified ArrayPool.</summary>
+    /// <param name="pool">Array pool.</param>
+    /// <param name="capacity">Initial capacity.</param>
+    public PooledMemoryStream(ArrayPool<byte> pool, int capacity = 4096)
+    {
+        if (pool == null)
+        {
+            Thrown.ArgumentNullException(nameof(pool));    
+        }
+        
+        _pool = pool;
+        _currentBuffer = _pool.Rent(capacity);
+        _length = 0;
+        CanWrite = true;
+    }
+
+    /// <summary>Initialize new object for reading.</summary>
+    /// <param name="data">Data that should be read.</param>
+    public PooledMemoryStream(byte[] data)
+    {
+        if (data == null)
+        {
+            Thrown.ArgumentNullException(nameof(data));
+        }
+        
+        _pool = null;
+        _currentBuffer = data;
+        _length = data.Length;
+        CanWrite = false;
+    }
+
+    /// <inheritdoc />
+    public override bool CanRead => true;
+
+    /// <inheritdoc />
+    public override bool CanSeek => true;
+
+    /// <inheritdoc />
+    public override bool CanWrite { get; }
+
+    /// <inheritdoc />
+    public override long Length => _length;
+
+    /// <inheritdoc />
+    public override long Position
+    {
+        get => _position;
+        set => _position = (int)value;
+    }
+
+    /// <inheritdoc />
+    public override void Flush()
+    {
+        CheckDisposed();
+    }
+
+    /// <summary>Set stream length.</summary>
+    /// <param name="value">New length.</param>
+    /// <exception cref="System.NotSupportedException">Stream is readonly.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Value is less than 0 or greater int.MaxValue.</exception>
+    public override void SetLength(long value)
+    {
+        CheckDisposed();
+        if (!CanWrite)
+        {
+            Thrown.NotSupportedException("Stream is readonly");
+        }
+
+        if (value > int.MaxValue)
+        {
+            Thrown.ArgumentOutOfRangeException(nameof(value), "Value should be least than int.MaxValue", value);
+        }
+
+        if (value < 0)
+        {
+            Thrown.ArgumentOutOfRangeException(nameof(value), "Value should be greater than 0", value);
+        }
+
+        _length = (int)value;
+        if (_currentBuffer == null || _currentBuffer.Length < _length)
+        {
+            ResizeBuffer(_length);
+        }
+
+        if (_position >= _length)
+        {
+            if (_length == 0)
+            {
+                _position = 0;
+            }
+            else
+            {
+                _position = _length - 1;
+            }
+        }
+    }
+
+    /// <summary>Ensure the buffer size.</summary>
+    /// <exception cref="System.NotSupportedException">Stream is readonly.</exception>
+    public void Reserve(int capacity)
+    {
+        CheckDisposed();
+        if (!CanWrite)
+        {
+            Thrown.NotSupportedException("Stream is readonly");
+        }
+
+        if (capacity > _currentBuffer.Length)
+        {
+            ResizeBuffer(capacity);
+        }
+    }
+
+    /// <summary>Create newly allocated buffer and copy the stream data.</summary>
+    public byte[] ToArray()
+    {
+        CheckDisposed();
+        byte[] ret = new byte[_length];
+        byte[] result = GC.AllocateUninitializedArray<byte>(_length);
+        Buffer.BlockCopy(_currentBuffer, 0, result, 0, _length);
+        return ret;
+    }
+
+    /// <summary>Create ArraySegment for current stream data without allocation buffer.</summary>
+    /// <remarks>After disposing stream, manipulating returned value (read or write) may cause undefined behavior.</remarks>
+    public ArraySegment<byte> ToUnsafeArraySegment()
+    {
+        CheckDisposed();
+        return new ArraySegment<byte>(_currentBuffer, 0, _length);
+    }
+    
+    /// <summary>Get data as Span.</summary>
+    /// <remarks>You must not use returned value outside of stream's lifetime</remarks>
+    public ReadOnlySpan<byte> ToSpanUnsafe()
+    {
+        CheckDisposed();
+        if (_currentBuffer == null || _length <= 0)
+        {
+            return Span<byte>.Empty;
+        }
+
+        return _currentBuffer.AsSpan(0, _length);
+    }
+
+    /// <summary>Get data as Span.</summary>
+    /// <remarks>You must not use returned value outside of stream's lifetime.</remarks>
+    public ReadOnlyMemory<byte> ToMemoryUnsafe()
+    {
+        CheckDisposed();
+        if (_currentBuffer == null || _length <= 0)
+        {
+            return Memory<byte>.Empty;
+        }
+
+        return _currentBuffer.AsMemory(0, _length);
+    }
+
+    /// <inheritdoc />
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        CheckDisposed();
+        int oldValue = _position;
+        switch ((int)origin)
+        {
+            case (int)SeekOrigin.Begin:
+                _position = (int)offset;
+                break;
+            
+            case (int)SeekOrigin.End:
+                _position = _length - (int)offset;
+                break;
+            
+            case (int)SeekOrigin.Current:
+                _position += (int)offset;
+                break;
+            
+            default:
+                Thrown.InvalidOperationException("Wrong SeekOrigin");
+                break;
+        }
+
+        if (_position < 0 || _position > _length)
+        {
+            _position = oldValue;
+            Thrown.IndexOutOfRangeException();
+        }
+
+        return _position;
+    }
+
+    /// <inheritdoc />
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        CheckDisposed();
+        int length = count > _length - _position ? _length - _position : count;
+        if (length <= 0)
+        {
+            return 0;
+        }
+        
+        Buffer.BlockCopy(_currentBuffer, _position, buffer, offset, length);
+        _position += length;
+        return length;
+    }
+
+    /// <inheritdoc />
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        CheckDisposed();
+        if (!CanWrite)
+        {
+            Thrown.NotSupportedException("Stream is readonly");
+        }
+
+        int endOffset = _position + count;
+        if (_currentBuffer == null || endOffset > _currentBuffer.Length)
+        {
+            ResizeBuffer(endOffset * 2);
+        }
+
+        Buffer.BlockCopy(buffer, offset, _currentBuffer, _position, count);
+        if (endOffset > _length)
+        {
+            _length = endOffset;
+        }
+
+        _position = endOffset;
+    }
+
+    /// <summary>Shrink internal buffer by re-allocating memory.</summary>
+    /// <param name="newCapacity">New size.</param>
+    /// <exception cref="System.NotSupportedException">Stream is readonly.</exception>
+    public void Shrink(int newCapacity)
+    {
+        CheckDisposed();
+        if (!CanWrite)
+        {
+            Thrown.NotSupportedException("Stream is readonly");
+        }
+
+        if (_currentBuffer == null)
+        {
+            return;
+        }
+
+        if (_currentBuffer.Length > newCapacity)
+        {
+            ResizeBuffer(newCapacity);
+        }
+
+        if (newCapacity <= _length)
+        {
+            _length = newCapacity;
+        }
+    }
+    
+    /// <inheritdoc />
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (_pool != null && _currentBuffer != null)
+        {
+            _pool.Return(_currentBuffer);
+            _currentBuffer = null!;
+        }
+
+        _length = 0;
+        _position = 0;
+        _isDisposed = true;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [System.Diagnostics.CodeAnalysis.MemberNotNull(nameof(_currentBuffer))]
+    private void ResizeBuffer(int minimumRequired)
+    {
+        byte[] tmp = _pool!.Rent(minimumRequired);
+        if (_currentBuffer != null)
+        {
+            Buffer.BlockCopy(_currentBuffer, 0, tmp, 0,
+            _currentBuffer.Length < tmp.Length ? _currentBuffer.Length : tmp.Length);
+            _pool.Return(_currentBuffer);
+        }
+
+        _currentBuffer = tmp;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void CheckDisposed()
+    {
+        if (_isDisposed)
+        {
+            Thrown.ObjectDisposedException(nameof(PooledMemoryStream));
+        }
+    }
+}
