315e386def66a936c72b395a61b23be8caafba09
Pavel Gribov <pavel@gribov.pro>
2024-01-16 03:24:06 +0300

    Added ISagaConsumer

--- src/EventSourcing.Net.Abstractions/ServiceRegistration/IEventConsumer.cs
+++ src/EventSourcing.Net.Abstractions/ServiceRegistration/IEventConsumer.cs
@@ -16,3 +16,17 @@ public interface IEventConsumer<TId, in TEventType> where TEventType : IEvent
     /// <returns>Awaitable task.</returns>
     Task Consume(IEventEnvelope<TId, TEventType> envelope);
 }
+
+/// <summary>
+/// Implementation of event consumption method.
+/// </summary>
+/// <typeparam name="TEventType">Type of event/</typeparam>
+public interface ISagaConsumer<TId, in TEventType> where TEventType : IEvent
+{
+    /// <summary>
+    /// Consume specific event by saga.
+    /// </summary>
+    /// <param name="envelope">Envelope with information about event.</param>
+    /// <returns>Awaitable task.</returns>
+    Task Consume(IEventEnvelope<TId, TEventType> envelope);
+}
